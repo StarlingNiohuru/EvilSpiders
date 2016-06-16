eed9732aebc8c4d9a349058bfb6d8b1303f7eba6
starling <starling@localhost>
2016-06-16 09:45:32 +0800

    kireina test

---
+++ EvilSpiders/spiders/Kireina.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from EvilSpiders.items import KireinaItem
+
+class KireinaSpider(scrapy.Spider):
+    name = "Kireina"
+    allowed_domains = ["http://kireina-megami.blog.jp"]
+    start_urls = (
+        'http://kireina-megami.blog.jp/archives/54453388.html',
+    )
+
+    def parse(self, response):
+        for sel in response.xpath('//div[@align="center"]/a'):
+            item = KireinaItem()
+            item['name'] = sel.xpath('@title').extract()[0]
+            item['image_url'] = sel.xpath('@href').extract()[0]
+            yield item
+        #fpath = 'midori_satsuki'
+        #with open(fpath, 'wb') as f:
+        #    res=urllib2.urlopen(item['image_url')
+        #    f.write(res.read())
+        #    f.close()
