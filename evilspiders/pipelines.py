6933c62e0bff8ee60b35a119718a3f4988c140c5
starling <starling@localhost>
2016-06-18 10:09:22 +0800

    image

--- evilspiders/pipelines.py
+++ evilspiders/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import urllib2
+import os
 
-class EvilspidersPipeline(object):
+class ImagePipeline(object):
+
+    def __init__(self):
+        self.path = '/home/starling/testpath/'
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
     def process_item(self, item, spider):
+        fname = self.path+item['name']+'.jpg'
+        with open(fname,'wb') as fw:
+            res = urllib2.urlopen(item['image_url'])
+            fw.write(res.read())
+            fw.close()
         return item
